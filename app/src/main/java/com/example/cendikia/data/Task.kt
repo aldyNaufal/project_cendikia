3ef6b9ec8b0467f64246b4dc4f52b7d49882c15e
Aldy Naufal <aldy.naufal02@gmail.com>
2024-11-21 13:36:10 +0700

    second push

--- app/src/main/java/com/example/cendikia/data/Task.kt
+++ app/src/main/java/com/example/cendikia/data/Task.kt
@@ -2,6 +2,8 @@ package com.example.cendikia.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
+
 
 @Entity(tableName = "task")
 data class Task(
@@ -10,4 +12,5 @@ data class Task(
     val name: String,
     val isCompleted: Boolean = false,
     val timestamp: Long = System.currentTimeMillis()
-)
+) : Serializable
+
